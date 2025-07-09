6f653f4f5bcdaca47d8488587d6e81b5d44dab14
Gowtham s <gowthamsk60@gmail.com>
2025-07-09 21:25:41 +0530

    Update basic1.java

--- basic1.java
+++ basic1.java
@@ -6,3 +6,14 @@ public class basic1 {
         System.out.println(args[0] + " technology " + args[1]);
     }
 }
+/*
+Write a Program that accepts two Strings as command line arguments and generate 
+the output in the required format. 
+Example1) 
+If the two command line arguments are Wipro and Bangalore then the output generated should 
+be Wipro Technologies Bangalore. 
+Example2) 
+If the command line arguments are ABC and Mumbai then the output generated should be ABC 
+Technologies Mumbai 
+[Note: It is mandatory to pass two arguments in command line]
+    */
