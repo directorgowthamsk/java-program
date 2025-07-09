9460904b747a0a96b510d25a06d259e72e5765e0
Gowtham s <gowthamsk60@gmail.com>
2025-07-09 21:27:42 +0530

    Update basic3.java

--- basic3.java
+++ basic3.java
@@ -5,3 +5,10 @@ public class basic3 {
         System.out.println("The sum of"+a+"and"+b+"is"+(a+b));
     }
 }
+/*
+ Write a Program to accept two integers as command line arguments and print the sum 
+of the two numbers 
+Example1) 
+C:\>java Sample 10 20 
+O/P Expected : The sum of 10 and 20 is 30
+    */
