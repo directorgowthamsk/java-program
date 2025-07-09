cede1505c6fcf842058703614f9da497278c775a
Gowtham s <gowthamsk60@gmail.com>
2025-07-09 22:06:59 +0530

    Update if5.java

--- if5.java
+++ if5.java
@@ -19,3 +19,18 @@ public class if5 {
         }
 }
 }
+/*
+Write a program to accept gender ("Male" or "Female") and age and print the 
+percentage of interest based on the given conditions. 
+Test Case 1 
+If the gender is 'Female' and age is between 1 and 58, the percentage of interest is 
+8.2%. 
+Test Case 2 
+If the gender is 'Female' and age is between 59 and 100, the percentage of interest is 
+9.2%. 
+Test Case 3 
+If the gender is 'Male' and age is between 1 and 58, the percentage of interest is 8.4%. 
+Test Case 4 
+If the gender is 'Male' and age is between 59 and 100, the percentage of interest is 
+10.5%.
+    */
