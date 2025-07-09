51d52309057f9c987829f11e3a575627a350c8d6
Gowtham s <gowthamsk60@gmail.com>
2025-07-09 22:08:30 +0530

    Update switch1.java

--- switch1.java
+++ switch1.java
@@ -28,3 +28,22 @@ public class switch1{
         }
     }
 }
+/*
+Write a program to receive a color code from the user (an Alphabhet). The program 
+should then print the color name, based on the color code given. 
+The following are the color codes and their corresponding color names. 
+R->Red, B->Blue, G->Green, O->Orange, Y->Yellow, W->White. 
+If color code provided by the user is not valid then print "Invalid Code". 
+Test Case 1 
+Input  
+Output : Green 
+Test Case 2 
+Input  
+Output : Blue 
+Test Case 3 
+Input  
+: G 
+: B 
+: M 
+Output : Invalid Code
+    */
