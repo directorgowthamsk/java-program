4984aed9b494809d0f5a07ab2c3c486422a291b2
Gowtham s <gowthamsk60@gmail.com>
2025-07-09 22:12:17 +0530

    Update forloop5.java

--- forloop5.java
+++ forloop5.java
@@ -14,17 +14,20 @@ public class forloop5 {
     }
 }
 /*
-Write a program to accept gender ("Male" or "Female") and age and print the 
-percentage of interest based on the given conditions. 
+ 
+Write a program to print the Floyds Triangle 
+ 
 Test Case 1 
-If the gender is 'Female' and age is between 1 and 58, the percentage of interest is 
-8.2%. 
+Input  : 3 
+Output  :  1 
+   2 3 
+   4 5 6 
+ 
 Test Case 2 
-If the gender is 'Female' and age is between 59 and 100, the percentage of interest is 
-9.2%. 
-Test Case 3 
-If the gender is 'Male' and age is between 1 and 58, the percentage of interest is 8.4%. 
-Test Case 4 
-If the gender is 'Male' and age is between 59 and 100, the percentage of interest is 
-10.5%.
+Input  : 6 
+Output  :  1 
+   2   3 
+   4   5    6 
+   7   8    9   10 
+   11 12 13  14  15
     */
