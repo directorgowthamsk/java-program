16948701b8a6737353da957a4912547f1643362c
Gowtham s <gowthamsk60@gmail.com>
2025-07-09 21:29:03 +0530

    Update Forloop1.java

--- Forloop1.java
+++ Forloop1.java
@@ -6,3 +6,18 @@ public class Forloop1 {
     }
 }
 
+/*
+Write a program to check if a given integer number is Positive, Negative, or Zero. 
+Test Case 1 
+Input 
+: 1 
+Output : Positive Number 
+Test Case 2 
+Input 
+: -125 
+Output : Negative Number 
+Test Case 3 
+Input 
+: 0 
+Output : Zero
+     */
