2c6b840c09291b70df35717bc3753556e114008a
Gowtham s <gowthamsk60@gmail.com>
2025-07-09 21:26:52 +0530

    Update basic2.java

--- basic2.java
+++ basic2.java
@@ -4,3 +4,10 @@ public class basic2 {
         System.out.println(" Welcome  " + args[0]);
     }
 }
+/*
+ Write a Program to accept a String as a command line argument and print a Welcome 
+message as given below. 
+Example1) 
+C:\> java Sample John 
+O/P Expected : Welcome John
+    */
